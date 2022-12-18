0a3444aff935b855ce07934dbe6c65e82b857886
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2022-12-18 14:40:05 +0300

    Update CardWithDeliveryTest.java

--- src/test/java/CardWithDeliveryTest.java
+++ src/test/java/CardWithDeliveryTest.java
@@ -1,3 +1,5 @@
+package ru.netology.test;
+
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.apache.commons.lang3.time.DateUtils;
@@ -6,8 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -15,8 +18,9 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardWithDeliveryTest {
-    SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-    Date date = new Date();
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @BeforeEach
     public void eachTest() {
@@ -28,129 +32,129 @@ public class CardWithDeliveryTest {
 
     @Test
     void scheduleDeliverySuccessfullySingleSurname() {
-        Date newDate = DateUtils.addDays(date, 3);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(3);
+
         $("[data-test-id=city] input").setValue("Волгоград");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + value), Duration.ofMillis(15000));
+        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
     @Test
     void scheduleDeliverySuccessfullyDoubleSurnameWithSpace() {
-        Date newDate = DateUtils.addDays(date, 4);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(4);
+
         $("[data-test-id=city] input").setValue("Москва");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
-        $("[data-test-id=name] input").setValue("Двойная Фамилия Имя");
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Двойная-Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + value), Duration.ofMillis(15000));
+        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
     @Test
     void scheduleDeliverySuccessfullyDoubleHyphenatedName() {
-        Date newDate = DateUtils.addDays(date, 5);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(5);
+
         $("[data-test-id=city] input").setValue("Санкт-Петербург");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя-Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + value), Duration.ofMillis(15000));
+        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
     @Test
     void scheduleDeliverySuccessfullyTenthDay() {
-        Date newDate = DateUtils.addDays(date, 10);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(10);
+
         $("[data-test-id=city] input").setValue("Волгоград");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + value), Duration.ofMillis(15000));
+        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
     @Test
     void negativeScheduleDeliveryNotACity() {
-        Date newDate = DateUtils.addDays(date, 10);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(10);
+
         $("[data-test-id=city] input").setValue("деревня");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id=city].input_invalid .input__sub")).shouldHave(exactText("Доставка в выбранный город недоступна"));
+        $(By.cssSelector("[data-test-id=city].input_invalid .input__sub")).shouldHave(exactText("Доставка в выбранный город недоступна")).shouldBe(Condition.visible);
     }
 
     @Test
     void negativeScheduleDeliveryZeroValueCity() {
-        Date newDate = DateUtils.addDays(date, 10);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(3);
+
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id=city].input_invalid .input__sub")).shouldHave(exactText("Поле обязательно для заполнения"));
+        $(By.cssSelector("[data-test-id=city].input_invalid .input__sub")).shouldHave(exactText("Поле обязательно для заполнения")).shouldBe(Condition.visible);
     }
 
     @Test
     void negativeScheduleDeliveryTomorrow() {
-        Date newDate = DateUtils.addDays(date, 1);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(1);
+
         $("[data-test-id=city] input").setValue("Волгоград");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id='date'] span.input__sub")).shouldHave(exactText("Заказ на выбранную дату невозможен"));
+        $(By.cssSelector("[data-test-id='date'] span.input__sub")).shouldHave(exactText("Заказ на выбранную дату невозможен")).shouldBe(Condition.visible);
     }
 
     @Test
     void negativeScheduleDeliveryYesterday() {
-        Date newDate = DateUtils.addDays(date, -1);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(-1);
+
         $("[data-test-id=city] input").setValue("Волгоград");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id='date'] span.input__sub")).shouldHave(exactText("Заказ на выбранную дату невозможен"));
+        $(By.cssSelector("[data-test-id='date'] span.input__sub")).shouldHave(exactText("Заказ на выбранную дату невозможен")).shouldBe(Condition.visible);
     }
 
     @Test
@@ -163,130 +167,130 @@ public class CardWithDeliveryTest {
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id='date'] span.input__sub")).shouldHave(exactText("Неверно введена дата"));
+        $(By.cssSelector("[data-test-id='date'] span.input__sub")).shouldHave(exactText("Неверно введена дата")).shouldBe(Condition.visible);
     }
 
     @Test
     void negativeScheduleDeliveryNameLatinAlphabet() {
-        Date newDate = DateUtils.addDays(date, 5);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(5);
+
         $("[data-test-id=city] input").setValue("Рязань");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Last Name First Name");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")).shouldBe(Condition.visible);
     }
 
     @Test
     void negativeScheduleDeliveryNameSpecialCharacters() {
-        Date newDate = DateUtils.addDays(date, 5);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(5);
+
         $("[data-test-id=city] input").setValue("Рязань");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("#$%");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")).shouldBe(Condition.visible);
     }
 
     @Test
     void negativeScheduleDeliveryZeroValueName() {
-        Date newDate = DateUtils.addDays(date, 5);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(3);
+
         $("[data-test-id=city] input").setValue("Рязань");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).shouldHave(exactText("Поле обязательно для заполнения"));
+        $(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).shouldHave(exactText("Поле обязательно для заполнения")).shouldBe(Condition.visible);
     }
 
     @Test
     void negativeScheduleDeliveryPhoneWithoutPlus() {
-        Date newDate = DateUtils.addDays(date, 5);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(3);
+
         $("[data-test-id=city] input").setValue("Рязань");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")).shouldBe(Condition.visible);
     }
 
     @Test
     void negativeScheduleDeliveryPhoneTenDigits() {
-        Date newDate = DateUtils.addDays(date, 5);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(5);
+
         $("[data-test-id=city] input").setValue("Рязань");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+7800555555");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")).shouldBe(Condition.visible);
 
     }
 
     @Test
     void negativeScheduleDeliveryPhoneTwelveDigits() {
-        Date newDate = DateUtils.addDays(date, 5);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(5);
+
         $("[data-test-id=city] input").setValue("Рязань");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+780055555500");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")).shouldBe(Condition.visible);
 
     }
 
     @Test
     void negativeScheduleDeliveryZeroValuePhone() {
-        Date newDate = DateUtils.addDays(date, 5);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(5);
+
         $("[data-test-id=city] input").setValue("Рязань");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).shouldHave(exactText("Поле обязательно для заполнения"));
+        $(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).shouldHave(exactText("Поле обязательно для заполнения")).shouldBe(Condition.visible);
 
     }
 
     @Test
     void negativeScheduleDeliveryCheckboxNotMarked() {
-        Date newDate = DateUtils.addDays(date, 10);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(30);
+
         $("[data-test-id=city] input").setValue("Волгоград");
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $(By.className("button")).click();
@@ -296,18 +300,17 @@ public class CardWithDeliveryTest {
 
     @Test
     void scheduleDeliveryDropDownListOfCities() {
-        Date newDate = DateUtils.addDays(date, 10);
-        String value =  formatter.format(newDate);
+        String planningDate = generateDate(50);
         $("[data-test-id=city] input").setValue("Пе");
         $x("//span[contains(text(),'Камчатский')]").click();
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(value);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Фамилия Имя");
         $("[data-test-id=phone] input").setValue("+78005555550");
         $("[data-test-id=agreement]").click();
         $(By.className("button")).click();
 
-        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + value), Duration.ofMillis(15000));
+        $("[data-test-id=notification]").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 }
